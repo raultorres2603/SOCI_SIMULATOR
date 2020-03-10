86063ca30a80d3a3da6a34ff4557f36da38c83dc
Raul Torres <raultorres2603@gmail.com>
2020-03-10 20:02:49 +0100

    JQuery added

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $("#principal-content").animate({
+    opacity: "1"
+  }, 2000);
+});
+
+function next() {
+
+}
--- register_login.php
+++ register_login.php
@@ -9,31 +9,33 @@ if (isset($_SESSION["user"])) {
    <head>
      <meta charset="utf-8">
      <title>International Simulator</title>
+     <!--//////////////////JQUERY/////////////////-->
+     <script src="./ext/JQuery.js"></script>
+     <!--/////////////////////////////////////////-->
      <!--/////////////BOOTSTRAP///////////////////-->
      <link rel="stylesheet" href="./css/Bootstrap/css/bootstrap.min.css">
      <script src="./css/Bootstrap/js/bootstrap.min.js"></script>
      <!--/////////////////////////////////////////-->
      <link rel="stylesheet" href="./css/main.css">
-     <!--//////////////////JQUERY/////////////////-->
-     <script src="./ext/JQuery.js"></script>
-     <!--/////////////////////////////////////////-->
    </head>
    <body>
-     <div class="container-fluid">
+     <div class="container-fluid" id="principal-content">
 	<div class="row">
 		<div class="col-md-12">
       <h1 class="title transitions">REGISTRO</h1>
       <h1 class="sub-title transitions">Simulador de Operaciones de Comercio Internacional</h1>
 			<div class="row">
 				<div class="col-md-6">
-          <label class="label_form transitions">Nombre:</label><input type="text" class="button-reg-log transitions" placeholder="Nombre" maxlength="10" id="name" name="name">
+          <label class="label_form transitions">Nombre:</label><input type="text" class="button-reg-log transitions" maxlength="10" id="name" name="name">
 				</div>
 				<div class="col-md-6">
-          <label class="label_form transitions">Apellido:</label><input type="text" class="button-reg-log transitions" placeholder="Apellido" maxlength="10" id="surname" name="surname">
+          <label class="label_form transitions">Apellido:</label><input type="text" class="button-reg-log transitions" maxlength="30" id="surname" name="surname">
 				</div>
 			</div>
+      <button type="button" class="button-register-login transitions">NEXT</button>
 		</div>
 	</div>
 </div>
+<script src="./js/main.js"></script>
    </body>
  </html>
