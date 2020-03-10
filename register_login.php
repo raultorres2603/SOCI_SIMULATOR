0dc892b511d24810f18cd28b6dbcae67e108f9ac
Raul Torres <raultorres2603@gmail.com>
2020-03-10 19:24:29 +0100

    Register-HTML

--- register_login.php
+++ register_login.php
@@ -10,10 +10,8 @@ if (isset($_SESSION["user"])) {
      <meta charset="utf-8">
      <title>International Simulator</title>
      <!--/////////////BOOTSTRAP///////////////////-->
-     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+     <link rel="stylesheet" href="./css/Bootstrap/css/bootstrap.min.css">
+     <script src="./css/Bootstrap/js/bootstrap.min.js"></script>
      <!--/////////////////////////////////////////-->
      <link rel="stylesheet" href="./css/main.css">
      <!--//////////////////JQUERY/////////////////-->
@@ -24,10 +22,14 @@ if (isset($_SESSION["user"])) {
      <div class="container-fluid">
 	<div class="row">
 		<div class="col-md-12">
+      <h1 class="title transitions">REGISTRO</h1>
+      <h1 class="sub-title transitions">Simulador de Operaciones de Comercio Internacional</h1>
 			<div class="row">
 				<div class="col-md-6">
+          <label class="label_form transitions">Nombre:</label><input type="text" class="button-reg-log transitions" placeholder="Nombre" maxlength="10" id="name" name="name">
 				</div>
 				<div class="col-md-6">
+          <label class="label_form transitions">Apellido:</label><input type="text" class="button-reg-log transitions" placeholder="Apellido" maxlength="10" id="surname" name="surname">
 				</div>
 			</div>
 		</div>
